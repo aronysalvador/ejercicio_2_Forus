e12f3422ba5f8fe2e561a38d5f3754a5d26aefbf
Arony Salvador <salvadorarony@gmail.com>
2020-06-18 14:53:34 -0400

    Cambios en el Despliegue

--- config/axios.js
+++ config/axios.js
@@ -1,10 +1,8 @@
 import axios from 'axios';
 
-//axios.defaults.headers.post["Content-Type"] = "application/json";
-
 const  clienteAxios = axios.create({
-    baseURL: 'https://cors-anywhere.herokuapp.com/https://sistemas.forus.cl/forus/challenge/dummy-api/producto/',
-   // baseURL: 'https://cors-anywhere.herokuapp.com/https://jsonplaceholder.typicode.com/posts',
+
+    baseURL:  process.env.API_BACK,
     headers: {
         "Access-Control-Allow-Origin": "*",
         "Content-type": "application/json; charset=UTF-8"
